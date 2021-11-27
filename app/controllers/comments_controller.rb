ac6d228b59a530e1bcbfaad91453a71fec5477a8
blfernandezGit <blfernandez@up.edu.ph>
2021-11-27 10:36:02 +0800

    show update delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,10 +13,33 @@ class CommentsController < ApplicationController
         @comment = @article.comments.build(comment_params)
     
         if @comment.save
-            redirect_to show_article_path(@article)
+            redirect_to article_comment_path(@article.id, @comment.id)
         else
             render :new
         end
+    end
+
+    def show
+        @comment = Comment.find(params[:id])
+    end
+
+    def edit
+        @comment = Comment.find(params[:id])
+    end
+
+    def update
+        @comment = Comment.find(params[:id])
+        if @comment.update(comment_params)
+            redirect_to article_comment_path(@article.id, @comment.id)
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @comment = Comment.find(params[:id])
+        @comment.destroy
+        redirect_to article_path(@article.id)
     end
     
     private
